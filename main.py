fd6c839cd1c8c677382af27498cca72f85dfdf7a
Apoorva <hapoorvasharma@gmail.com>
2026-08-11 12:11:52 +0530

    feat: add close command to the main chat loop

--- main.py
+++ main.py
@@ -21,6 +21,13 @@ def main():
             success = system_control.open_app(app_name)
             if not success:
                 print(f"Sorry, I don't know how to open '{app_name}' yet.")
+
+        elif user_input.startswith("close "):
+            app_name = user_input.replace("close ", "", 1)
+            success = system_control.close_app(app_name)
+            if not success:
+                print(f"Sorry, I couldn't close '{app_name}'.")
+
         else:
             print("Sorry, I don't understand that command yet.")
 
